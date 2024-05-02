e6787d0f9fbc43de666885eb6be2bb3d6ee6c689
Ali_Khan <160100672+Alikhani205@users.noreply.github.com>
2024-05-02 03:05:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//let totalmark: number = 850;//
+//let obtainmark: number = 540
+//let percentage: number = (obtainmark/totalmark) * 100;
+//console.log(percentage);//
+var percentage = 63.52;
+if (function (percentage) { return 63.52; }) {
+    console.log('your grade is B');
+}
+else if (percentage < 63.52) {
+    console.log('your grade is C');
+}
+else if (percentage < 50) {
+    console.log('you are passed');
+}
